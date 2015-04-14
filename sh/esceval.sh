715b9f9b8a7724718e40d30c6e7355918114227f
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-04-14 02:57:04 +0000

    Re-added sed version as a fallback for shells which don't support ${var%%word} and ${var#word syntax}.

--- sh/esceval.sh
+++ sh/esceval.sh
@@ -1,4 +1,4 @@
-# Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-04-07;
+# Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-04-14;
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public Licence as published by
@@ -15,41 +15,62 @@
 # or write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330 Boston MA 02111-1307 USA.
 
-# On 2015-04-13, this script was compatible with Bourne and POSIX shells.
+# On 2015-04-14, this script was compatible with Bourne and POSIX shells.
 # EXCEPT for the following exceptions:
 # Function declarations (some ancient Bourne shells don't support functions).
-# ${var%%word} is not supported in some shells (e.g. Solaris 10 /bin/sh).
-# ${var#word} is not supported in some shells (e.g. Solaris 10 /bin/sh).
 # I am currently unclear on the portability of the "type" command.
 
 if ! type esceval > /dev/null
 then
- esceval()
- {
-  case $# in 0) return 0; esac
-  (
+ # ${var%%word} and ${var#word} are not supported in old Bourne shells, such as
+ # the /bin/sh on Solaris 10 and earlier. So first we check if that syntax is
+ # working. If it is, we do the shell-only implementation. If it is not, we
+ # fall back onto a sed-dependent implementation.
+ if (eval 'echo ${A%%a} ${A#a}' 1>/dev/null 2>&1)
+ then
+  eval 'esceval()
+  {
+   case $# in 0) return 0; esac
+   # The rest of the function is in a subshell to keep the variable definitions
+   # from leaking into the shell environment this is being run in.
+   (
+    while true
+    do
+     printf "'\''"
+     UNESCAPED=$1
+     while true
+     do
+      case $UNESCAPED in
+      *\'\''*)
+       printf %s "${UNESCAPED%%\'\''*}"
+       printf "'"'\''"'"
+       UNESCAPED=${UNESCAPED#*\'\''}
+       ;;
+      *)
+       break
+      esac
+     done
+     printf %s "$UNESCAPED"
+     shift
+     case $# in 0) break; esac
+     printf "'\'' "
+    done
+    printf "'\''\n"
+   )
+  }'
+ else
+  esceval()
+  {
+   case $# in 0) return 0; esac
    while true
    do
     printf "'"
-    UNESCAPED=$1
-    while true
-    do
-     case $UNESCAPED in
-     *\'*)
-      printf %s "${UNESCAPED%%\'*}"
-      printf "'\''"
-      UNESCAPED=${UNESCAPED#*\'}
-      ;;
-     *)
-      break
-     esac
-    done
-    printf %s "$UNESCAPED"
+    printf %s "$1" | sed "s/'/'\\\\''/g"
     shift
     case $# in 0) break; esac
     printf "' "
    done
    printf "'\n"
-  )
- }
+  }
+ fi
 fi
