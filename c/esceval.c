62721fe78d36282347175af0cc5498f57a74554b
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2016-01-01 08:26:09 +0000

    Relicensed to ISC License
    Many months of ethics contemplation went into this change. Many.

--- c/esceval.c
+++ c/esceval.c
@@ -1,21 +1,18 @@
-/*
- * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-02-24;
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public Licence as published by
- * the Free Software Foundation, either version 3 of the licence or,
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for details.
- * 
- * You should've received a copy of the GNU General Public License
- * with this program. If not, see <http://www.gnu.org/licences/>,
- * or write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330 Boston MA 02111-1307 USA.
- */
+/*****************************************************************************\
+ * Copyright (C) 2015-02-24 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+\*****************************************************************************/
 
 #include <stdio.h>
 
