aaeacc5e81fc05b17450eb020453b9885e68c5cd
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2017-03-14 12:00:43 +0000

    Relicensed: Basically public domain dedication now.
    * Specifically, this is known alternatively as the Free Public License
      or as the Zero-clause BSD license. It's used by projects like Toybox.
    * More specifically, those licenses are the ISC license without the
      clause requiring that you keep the license and copyright notice.
    * Those license use the new "and/or" wording that the ISC eventually
      adopted: I'm sticking with the original ISC license wording of "and".
      This is NOT a change in meaning, as far as I interpret or intend it. I
      just agree with the OpenBSD team's sentiment that the original wording
      was legally fine, and that replacing "and" with "and/or" might cause
      more erroneous interpretations in other jurisdictions besides the USA.
    * TL;DR: In summary, I give this code altruistically to the world, as a
      public good, and demand/expect nothing in return.

--- sh/esceval.sh
+++ sh/esceval.sh
@@ -1,8 +1,7 @@
 # Copyright (C) 2015-02-24 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
 #
 # Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
+# purpose with or without fee is hereby granted.
 #
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
