f335a2b53da61d1d94e76133064d288f6d81c714
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2019-07-11 09:30:11 +0000

    Readability: four space indents

--- c/esceval.c
+++ c/esceval.c
@@ -19,50 +19,50 @@
 static
 void fail_if_eof(int result, char * arg0)
 {
- if(result == EOF)
- {
-  perror(arg0);
-  exit(EXIT_FAILURE);
- }
+    if(result == EOF)
+    {
+        perror(arg0);
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char * * argv)
 {
- char * arg0 = *argv;
- if(argc < 2)
- {
-  return EXIT_SUCCESS;
- }
- argv += 1;
- fail_if_eof(fputc('\'', stdout), arg0);
- for(;;)
- {
-  char * arg = *argv;
-  for(;;)
-  {
-   char c = *arg;
-   if(c == '\'')
-   {
-    fail_if_eof(fputs("'\\''", stdout), arg0);
-   }
-   else if(c)
-   {
-    fail_if_eof(fputc(c, stdout), arg0);
-   }
-   else
-   {
-    break;
-   }
-   arg += 1;
-  }
-  argv += 1;
-  if(!*argv)
-  {
-   break;
-  }
-  fail_if_eof(fputs("' '", stdout), arg0);
- }
- fail_if_eof(fputs("'\n", stdout), arg0);
- fail_if_eof(fflush(stdout), arg0);
- return EXIT_SUCCESS;
+    char * arg0 = *argv;
+    if(argc < 2)
+    {
+        return EXIT_SUCCESS;
+    }
+    argv += 1;
+    fail_if_eof(fputc('\'', stdout), arg0);
+    for(;;)
+    {
+        char * arg = *argv;
+        for(;;)
+        {
+            char c = *arg;
+            if(c == '\'')
+            {
+                fail_if_eof(fputs("'\\''", stdout), arg0);
+            }
+            else if(c)
+            {
+                fail_if_eof(fputc(c, stdout), arg0);
+            }
+            else
+            {
+                break;
+            }
+            arg += 1;
+        }
+        argv += 1;
+        if(!*argv)
+        {
+            break;
+        }
+        fail_if_eof(fputs("' '", stdout), arg0);
+    }
+    fail_if_eof(fputs("'\n", stdout), arg0);
+    fail_if_eof(fflush(stdout), arg0);
+    return EXIT_SUCCESS;
 }
