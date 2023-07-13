ae6dfafa28918befceaf1a174923c7c5204636da
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-07-13 08:48:54 +0000

    escevalid: command to validate esceval output
    Specifically, `escevalid` checks that each of its
    arguments is one or more outputs in `esceval`
    format which can be evaluated (with `eval` or
    similar) predictably: in particular, not only are
    they all proper single-quoted strings, but also if
    a single argument would evaluate to more than one
    single-quoted string, they are only separated by
    one or more spaces - this protects against the
    case where they are separated by a line feed,
    since everything on a new line would get evaluated
    as a new command by `eval` and the like (unless
    the previous line ended in a backslash, which
    esceval does not do).
    
    Years ago, I realized that certain uses of esceval
    could only be secure and reliable if they could
    check that the output was still safe to evaluate.
    I started both a C and /bin/sh implementation of
    `escevalid` back then, but didn't finish until now.

---
+++ c/escevalid.c
@@ -0,0 +1,81 @@
+/* SPDX-License-Identifier: 0BSD */
+/* Copyright 2017 Alexander Kozhevnikov <mentalisttraceur@gmail.com> */
+
+#include <stdlib.h> /* EXIT_SUCCESS, EXIT_FAILURE, exit */
+
+static
+char * require(char required, char * string)
+{
+    if(*string != required)
+    {
+        exit(EXIT_FAILURE);
+    }
+    return string + 1;
+}
+
+static
+char * require_closing_quote(char * string)
+{
+    for(;;)
+    {
+        char next = *string;
+        if(next == '\0')
+        {
+            exit(EXIT_FAILURE);
+        }
+        if(next == '\'')
+        {
+            return string + 1;
+        }
+        string += 1;
+    }
+}
+
+static
+char * skip_spaces(char * string)
+{
+    while(*string == ' ')
+    {
+        string += 1;
+    }
+    return string;
+}
+
+static
+void escevalid(char * unvalidated)
+{
+    unvalidated = skip_spaces(unvalidated);
+    while(*unvalidated != '\0')
+    {
+        unvalidated = require('\'', unvalidated);
+        unvalidated = require_closing_quote(unvalidated);
+        if(*unvalidated == ' ')
+        {
+            unvalidated = skip_spaces(unvalidated);
+        }
+        else if(*unvalidated != '\0')
+        {
+            unvalidated = require('\\', unvalidated);
+            unvalidated = require('\'', unvalidated);
+            if(*unvalidated == '\0')
+            {
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+int main(int argc, char * * argv)
+{
+    char * arg;
+    if(argc < 2)
+    {
+        return EXIT_SUCCESS;
+    }
+    argv += 1;
+    while((arg = *argv++))
+    {
+        escevalid(arg);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ sh/escevalid.sh
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: 0BSD
+# Copyright 2023 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+
+if ! escevalid >/dev/null 2>&1
+then
+    # Mutates: $unvalidated
+    _escevalid_skip_spaces()
+    {
+        while :
+        do
+            case $unvalidated in
+            ' '*)
+                unvalidated=${unvalidated#' '}
+            ;;
+            *)
+                break
+            esac
+        done
+    }
+
+    # Mutates: $unvalidated
+    # Exits current (sub)shell if there is no closing quote.
+    _escevalid_require_closing_quote()
+    {
+        while :
+        do
+            case $unvalidated in
+            \'*)
+                unvalidated=${unvalidated#\'}
+                break
+            ;;
+            '')
+                exit 1
+            ;;
+            *)
+                unvalidated=${unvalidated#?}
+            esac
+        done
+    }
+
+    # Mutates: $unvalidated
+    # Exits current (sub)shell if the next character is not the required one.
+    _escevalid_require()
+    {
+        case $unvalidated in
+        "$1"*)
+            unvalidated=${unvalidated#"$1"}
+        ;;
+        *)
+            exit 1
+        esac
+    }
+
+    escevalid()
+    (
+        for unvalidated
+        do
+            _escevalid_skip_spaces
+            while :
+            do
+                case $unvalidated in '') break; esac
+                _escevalid_require \'
+                _escevalid_require_closing_quote
+                case $unvalidated in
+                ' '*)
+                    _escevalid_skip_spaces
+                ;;
+                ?*)
+                    _escevalid_require \\
+                    _escevalid_require \'
+                    case $unvalidated in '') exit 1; esac
+                esac
+            done
+        done
+    )
+fi
+
+escevalid "$@"
