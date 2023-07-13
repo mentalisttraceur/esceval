c263eb86c4e1aaed42379a1887d349f0e4d25204
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-07-13 11:10:26 +0000

    esceval*.sh: remove polyfill guards
    This made sense if the scripts were meant to be
    pasted as polyfills, but as drop-in executables
    these is-esceval*-already-available checks would
    just recurse indefinitely.
    
    In principle, I increasingly think that most code
    should not polyfill things directly - it should
    just rely on those things existing, and it is the
    job of the system/runtime/packaging/compiler to
    add the polyfills.
    
    In my own use, I just have esceval* installed in
    my PATH at all times anyway, and I've somewhat
    moved on from the idea of building scripts for
    which need to be complex enough to need esceval*.
    It's possible it will come up, but seems unlikely.
    (And I would remember to come back here to grab
    the polyfill guards in any case.)
    
    For others, even if a script does copy code from
    esceval*.sh, the performance hit relative to the
    C implementation is only significant for huuuuge
    arguments or argument lists. So might as well
    just use the in-shell implementations even if a
    faster implementation is installed externally,
    at least until performance becomes a problem.

--- sh/escevalenv.sh
+++ sh/escevalenv.sh
@@ -1,32 +1,30 @@
 # SPDX-License-Identifier: 0BSD
 # Copyright 2017 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
 
-case `x= escevalenv x 2>&1 || :` in "''") :;; *)
-    escevalenv()
-    (
-        case $# in 0) exit 0; esac
+escevalenv()
+(
+    case $# in 0) exit 0; esac
+    while :
+    do
+        escaped=\'
+        unescaped=`printenv "$1" || :`
         while :
         do
-            escaped=\'
-            unescaped=`printenv "$1" || :`
-            while :
-            do
-                case $unescaped in
-                *\'*)
-                    escaped=$escaped${unescaped%%\'*}"'\''"
-                    unescaped=${unescaped#*\'}
-                ;;
-                *)
-                    break
-                esac
-            done
-            escaped=$escaped$unescaped\'
-            shift
-            case $# in 0) break; esac
-            printf '%s ' "$escaped" || exit 1
+            case $unescaped in
+            *\'*)
+                escaped=$escaped${unescaped%%\'*}"'\''"
+                unescaped=${unescaped#*\'}
+            ;;
+            *)
+                break
+            esac
         done
-        printf '%s\n' "$escaped" || exit 1
-    )
-esac
+        escaped=$escaped$unescaped\'
+        shift
+        case $# in 0) break; esac
+        printf '%s ' "$escaped" || exit 1
+    done
+    printf '%s\n' "$escaped" || exit 1
+)
 
 escevalenv "$@"
--- sh/escevalid.sh
+++ sh/escevalid.sh
@@ -1,78 +1,75 @@
 # SPDX-License-Identifier: 0BSD
 # Copyright 2023 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
 
-if ! escevalid >/dev/null 2>&1
-then
-    # Mutates: $unvalidated
-    _escevalid_skip_spaces()
-    {
-        while :
-        do
-            case $unvalidated in
-            ' '*)
-                unvalidated=${unvalidated#' '}
-            ;;
-            *)
-                break
-            esac
-        done
-    }
-
-    # Mutates: $unvalidated
-    # Exits current (sub)shell if there is no closing quote.
-    _escevalid_require_closing_quote()
-    {
-        while :
-        do
-            case $unvalidated in
-            \'*)
-                unvalidated=${unvalidated#\'}
-                break
-            ;;
-            '')
-                exit 1
-            ;;
-            *)
-                unvalidated=${unvalidated#?}
-            esac
-        done
-    }
-
-    # Mutates: $unvalidated
-    # Exits current (sub)shell if the next character is not the required one.
-    _escevalid_require()
-    {
+# Mutates: $unvalidated
+_escevalid_skip_spaces()
+{
+    while :
+    do
         case $unvalidated in
-        "$1"*)
-            unvalidated=${unvalidated#"$1"}
+        ' '*)
+            unvalidated=${unvalidated#' '}
         ;;
         *)
-            exit 1
+            break
         esac
-    }
+    done
+}
 
-    escevalid()
-    (
-        for unvalidated
+# Mutates: $unvalidated
+# Exits current (sub)shell if there is no closing quote.
+_escevalid_require_closing_quote()
+{
+    while :
+    do
+        case $unvalidated in
+        \'*)
+            unvalidated=${unvalidated#\'}
+            break
+        ;;
+        '')
+            exit 1
+        ;;
+        *)
+            unvalidated=${unvalidated#?}
+        esac
+    done
+}
+
+# Mutates: $unvalidated
+# Exits current (sub)shell if the next character is not the required one.
+_escevalid_require()
+{
+    case $unvalidated in
+    "$1"*)
+        unvalidated=${unvalidated#"$1"}
+    ;;
+    *)
+        exit 1
+    esac
+}
+
+escevalid()
+(
+    for unvalidated
+    do
+        _escevalid_skip_spaces
+        while :
         do
-            _escevalid_skip_spaces
-            while :
-            do
-                case $unvalidated in '') break; esac
+            case $unvalidated in '') break; esac
+            _escevalid_require \'
+            _escevalid_require_closing_quote
+            case $unvalidated in
+            ' '*)
+                _escevalid_skip_spaces
+            ;;
+            ?*)
+                _escevalid_require \\
                 _escevalid_require \'
-                _escevalid_require_closing_quote
-                case $unvalidated in
-                ' '*)
-                    _escevalid_skip_spaces
-                ;;
-                ?*)
-                    _escevalid_require \\
-                    _escevalid_require \'
-                    case $unvalidated in '') exit 1; esac
-                esac
-            done
+                case $unvalidated in '') exit 1; esac
+            esac
         done
-    )
-fi
+    done
+)
 
 escevalid "$@"
