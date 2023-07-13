1ac8255fd4e2feeb397587eed6db088faf4cdeb2
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-07-13 08:45:25 +0000

    esceval.sh: drop support for ancient shells
    The #!/bin/sh implementation doesn't need to
    support Solaris 10 and various systems that
    no one uses. More importantly, I no longer
    want to spend (waste) myself on that support.

--- sh/esceval.sh
+++ sh/esceval.sh
@@ -1,62 +1,32 @@
 # SPDX-License-Identifier: 0BSD
 # Copyright 2014 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
 
-# On 2019-07-11, this script was compatible with Bourne and POSIX shells.
-# EXCEPT for the following exceptions:
-# Function declarations (First appeared in SVR2 Bourne shells in 1984).
-
 case `esceval '' 2>&1 || :` in "''") :;; *)
- if (eval 'echo ${A%%a} ${A#a}' 1>/dev/null 2>&1)
- then
-  eval 'esceval()
-  {
-   case $# in 0) return 0; esac
-   (
-    while :
-    do
-     escaped=\'\''
-     unescaped=$1
-     while :
-     do
-      case $unescaped in
-      *\'\''*)
-       escaped=$escaped${unescaped%%\'\''*}"'"'\''"'"
-       unescaped=${unescaped#*\'\''}
-       ;;
-      *)
-       break
-      esac
-     done
-     escaped=$escaped$unescaped\'\''
-     shift
-     case $# in 0) break; esac
-     printf "%s " "$escaped" || return $?
-    done
-    printf "%s\n" "$escaped"
-   )
-  }'
- else
-  esceval()
-  {
-   case $# in 0) return 0; esac
-   (
-    b='\\'
-    while :
-    do
-     escaped=`
-      printf '%s\n' "$1" \
-      | sed "
-         s/'/'$b''/g
-         1 s/^/'/
-         $ s/$/'/
-        "
-     ` || return $?
-     shift
-     case $# in 0) break; esac
-     printf '%s ' "$escaped" || return $?
-    done
-    printf '%s\n' "$escaped"
-   )
-  }
- fi
+    esceval()
+    (
+        case $# in 0) exit 0; esac
+        while :
+        do
+            escaped=\'
+            unescaped=$1
+            while :
+            do
+                case $unescaped in
+                *\'*)
+                    escaped=$escaped${unescaped%%\'*}"'\''"
+                    unescaped=${unescaped#*\'}
+                ;;
+                *)
+                    break
+                esac
+            done
+            escaped=$escaped$unescaped\'
+            shift
+            case $# in 0) break; esac
+            printf '%s ' "$escaped" || return $?
+        done
+        printf '%s\n' "$escaped"
+    )
 esac
+
+esceval "$@"
